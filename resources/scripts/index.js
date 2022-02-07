7cd3aebce439b0f8c09024ba42d8f3a7745562af
SamuelUntener <75641765+SamuelUntener@users.noreply.github.com>
2022-02-06 20:40:56 -0500

    finished readme

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,12 +1,14 @@
+const { update } = require("lodash");
+
 // Elements
 const welcomeScreen = document.getElementById(`welcome-screen`);
-const gameScreen = ;
-const startGameButton = ;
-const userName = ;
-const userSelection = ;
-const goButton = ;
-const scoreParagraph = ;
-const gameHistoryParagraph = ;
+const gameScreen = document.getElementById(`game-screen`);
+const startGameButton = document.getElementById(`start-game-button`);
+const userName = document.getElementById(`username`);
+const userSelection = document.getElementById(`user-selection`);
+const goButton = document.getElementById(`go-button`);
+const scoreParagraph = document.getElementById(`score`);
+const gameHistoryParagraph = document.getElementById(`game-history`);
 
 // instantiate the game object from the `RockPaperScissors` class.
 let game;
@@ -26,14 +28,20 @@ function updateGameHistoryUI(){
 
 // start-game-button EventListener
 startGameButton.addEventListener(`click`, function () {
-  const username = 
+  const username = this.textContent;
+  const generateCPUResponse = document.getElementsByName (generateCPUResponse);
+  welcomeScreen.classList.remove(`d-none`);
+  gameScreen.classList.add(`d-none`);
   game = new RockPaperScissors(userName);
   // Complete
 });
 
 // go-button EventListener
 goButton.addEventListener(`click`, function () {
-  
+  const userSelection = this.textContent;
+  Play(userSelection);
+  update(updateScoreTallyUI());
+  update(updateGameHistoryUI());
 });
 
 // If you're doing the extra-credit, uncomment the below: reset-game-button
